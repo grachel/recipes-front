358697fcaaf46613655c4586f69546d09ae88ad2
grzesiek <grzesiek.rachel@hotmail.com>
2019-06-29 14:41:33 +0200

    new recipe functionality

--- src/components/Add/index.js
+++ src/components/Add/index.js
@@ -1,8 +1,44 @@
 import React from "react";
 import { compose } from "recompose";
+import { withStyles } from "@material-ui/core/styles";
+
+import Paper from '@material-ui/core/Paper';
 
 import { withAuthorization } from "../Session";
 import { withService } from "../Service";
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+    padding: theme.spacing.unit * 2,
+    ali: 'center',
+  },
+  button: {
+    margin: theme.spacing.unit,
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+  },
+  input: {
+    display: 'none',
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    width: '30%',
+    display: 'block',
+    margin: 'auto',
+    marginTop: '5%'
+  },
+  chip: {
+    margin: theme.spacing.unit,
+  },
+});
 
 export class Add extends React.Component {
   state = {
@@ -14,10 +50,14 @@ export class Add extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <main className="ui main text container">
-          add
+        <Paper className={classes.paper}>
+        </Paper>
+        <Paper className={classes.paper}>
+        </Paper>
         </main>
       </div>
     );
@@ -28,6 +68,7 @@ const condition = authUser => !!authUser;
 
 const AddPage = compose(
   withService,
+  withStyles(styles),
   withAuthorization(condition)
 )(Add);
 
