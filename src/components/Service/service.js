b4ec70d91945cb12f66f732efb7c8aec2ccadd75
grachel <grachel@softsystem.pl>
2019-09-18 11:30:01 +0200

    remove not used param

--- src/components/Service/service.js
+++ src/components/Service/service.js
@@ -4,7 +4,6 @@ import "firebase/database";
 import "firebase/storage";
 
 const config = {
-  url: "http://localhost:8080/",
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
